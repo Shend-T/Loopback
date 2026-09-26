1a2da5c935ef8f367a2242aa427febc99d053f85
Shend <sheend102@gmail.com>
2026-09-26 15:19:49 +0200

    organizations: isplatform field

---
+++ backend/Migrations/20260926131746_OrganizationsIsPlatform.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class OrganizationsIsPlatform : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPlatformOrganization",
+                table: "Organizations",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsPlatformOrganization",
+                table: "Organizations");
+        }
+    }
+}
--- backend/Models/Organization.cs
+++ backend/Models/Organization.cs
@@ -7,5 +7,6 @@ public class Organization
     public DateOnly CreatedAt { get; set; } = DateOnly.FromDateTime(DateTime.UtcNow);
     public bool IsDeleted { get; set; } = false;
     public DateTimeOffset? DeletedAt { get; set; }
+    public bool IsPlatformOrganization { get; set; } = false;
     public ICollection<User> Users { get; set; } = new List<User>();
 }
